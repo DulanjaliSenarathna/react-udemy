cf671ae47570da371d201c3b76cd8db31cd08e92
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-20 16:23:27 +0530

    Writing More Complex JSX Code

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,7 +1,6 @@
 function ExpenseItems() {
-    return <h1>
-        Expense Item!
-    </h1>
+    return (<div><div>28 March 2021</div>
+        <div><h2>Car Insurance</h2></div>
+        <div>$296.67</div></div>)
 }
-
 export default ExpenseItems;
