a41c2883f01abc7a4c645f85a5abfa2ba2e2b841
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-21 17:08:46 +0530

    Assignment done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItems from "./components/ExpenseItems";
-
+import Expenses from "./components/Expenses";
 function App() {
 
   const expenses = [
@@ -10,13 +9,8 @@ function App() {
   ]
   return (
     <div>
-      <h2>Let's get started!</h2>
-      
-      <ExpenseItems title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItems title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItems title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItems title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
-     
+      <h2>Let's get started!</h2>  
+      <Expenses items={expenses} />
     </div>
   );
 }
