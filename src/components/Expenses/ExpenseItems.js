0a3e3023bbca4e6227c3fffb8c68989021b70a96
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-22 22:46:54 +0530

    Organizing Component Files

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,4 +1,4 @@
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css'
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import Card from "./Card"
+import Card from "../UI/Card"
 import ExpenseItems from "./ExpenseItems"
 import './Expenses.css'
 function Expenses(props){
