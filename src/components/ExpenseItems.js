79d77ce1eefaf99149b8232eedf3a1f4110b5404
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-22 15:47:58 +0530

    The Concept of "Composition" ("children props")

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,14 +1,15 @@
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css'
 
 function ExpenseItems(props) {
 
-    return (<div className='expense-item'>
+    return (<Card className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
         </div>
         <div className='expense-item__price'>${props.amount}</div>
-        </div>)
+        </Card>)
 }
 export default ExpenseItems;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,14 @@
+import Card from "./Card"
 import ExpenseItems from "./ExpenseItems"
 import './Expenses.css'
 function Expenses(props){
      return (
-        <div className="expenses">
+    <Card className="expenses">
       <ExpenseItems title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
       <ExpenseItems title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
       <ExpenseItems title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
       <ExpenseItems title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
-     
-        </div>
+    </Card>
      )
 }
 
