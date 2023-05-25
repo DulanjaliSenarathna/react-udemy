3aabe48146b3134a3d9f4ac040dbaaff88f56574
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-25 20:33:48 +0530

    Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import ExpenseFilter from "./components/Expenses/ExpenseFilter";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {id: 1, title:'Toilet paper', amount : 296.67, date: new Date()},
+  {id: 2,title:'Car insurance', amount : 296.67, date: new Date()},
+  {id: 3,title:'New Desk wooden', amount : 296.67, date: new Date()},
+  {id: 4,title:'PEn Holder', amount : 296.67, date: new Date()}
+]
 function App() {
 
-  const expenses = [
-    {id: 1, title:'Toilet paper', amount : 296.67, date: new Date()},
-    {id: 2,title:'Car insurance', amount : 296.67, date: new Date()},
-    {id: 3,title:'New Desk wooden', amount : 296.67, date: new Date()},
-    {id: 4,title:'PEn Holder', amount : 296.67, date: new Date()}
-  ]
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSES)
 
   const addExpenseHandler = (expense) =>{
-    console.log('App.js');
-      console.log(expense);
+   setExpenses((prevExpenses) =>{
+    return [expense, ...prevExpenses]
+   });
   }
   return (
     <div>
