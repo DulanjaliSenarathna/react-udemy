9928b884cc14dc9affe8af6047ffb15315c3db39
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-06-11 20:09:38 +0530

    Adding a "User" Component

---
+++ 04-Practice Project/src/components/Users/AddUser.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddUser = (props) => {
+
+    const addUserHandler = (event) =>{
+        event.preventDefault();
+    }
+  return (
+    <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text'></input>
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number'></input>
+        <button type='submit'>Add User</button>
+    </form>
+  )
+}
+
+export default AddUser
