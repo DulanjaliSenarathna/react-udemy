207bde4f5b2ff80bd56d6e1fdad8e0502fa05834
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-08-01 22:43:39 +0530

    Introducing React Context (Context API)

--- 06-usereducer-starting-project/src/App.js
+++ 06-usereducer-starting-project/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,13 +29,19 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    
+      <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn
+      }}
+      >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+      </AuthContext.Provider>
+    
   );
 }
 
