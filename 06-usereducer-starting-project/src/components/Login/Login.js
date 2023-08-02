de361814f546b8571aeabb230aa738196d2ab9b5
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-08-02 22:15:00 +0530

    Building & Using a Custom Context Provider Component

--- 06-usereducer-starting-project/src/components/Login/Login.js
+++ 06-usereducer-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state,action) =>{
 
@@ -44,6 +45,8 @@ const Login = (props) => {
     value:'',
     isValid:null
   });
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -98,7 +101,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.isValid);
+    authCtx.onLogin(emailState.value, passwordState.isValid);
   };
 
   return (
