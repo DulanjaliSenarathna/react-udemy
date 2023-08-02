537ad97514ef31425ef13cc772b39e2d68c9b9e4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-08-02 12:20:18 +0530

    Making Context Dynamic

--- 06-usereducer-starting-project/src/App.js
+++ 06-usereducer-starting-project/src/App.js
@@ -32,10 +32,11 @@ function App() {
     
       <AuthContext.Provider
       value={{
-        isLoggedIn: isLoggedIn
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler
       }}
       >
-      <MainHeader onLogout={logoutHandler} />
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
--- 06-usereducer-starting-project/src/components/MainHeader/Navigation.js
+++ 06-usereducer-starting-project/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import React,{useContext} from 'react';
 import classes from './Navigation.module.css';
 import AuthContext from '../../store/auth-context';
 
-const Navigation = (props) => {
+const Navigation = () => {
 
   const ctx = useContext(AuthContext);
   return (
@@ -22,7 +22,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
