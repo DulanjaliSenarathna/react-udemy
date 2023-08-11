50d659c087d93126da6d198bff633206a21aad0c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-08-11 20:46:09 +0530

    Adding Individual Meal Items & Displaying Them

--- Section 12/src/components/Meals/AvailableMeals.js
+++ Section 12/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -30,12 +32,14 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal =><li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map(meal =><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>);
   return (
     <section className={classes.meals}>
+        <Card>
         <ul>
             {mealsList}
         </ul>
+        </Card>
     </section>
   )
 }
---
+++ Section 12/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MealItemForm = () => {
+  return (
+    <div>MealItemForm</div>
+  )
+}
+
+export default MealItemForm
