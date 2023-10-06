856d4a0aac36623b1743e44a6dd7716f3c204123
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-10-06 16:55:42 +0530

    Using async / await

--- Section 15 - Sending http requests/src/App.js
+++ Section 15 - Sending http requests/src/App.js
@@ -7,11 +7,10 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/').then((response) => {
-      return response.json();
-    }).then((data) => {
-
+  async function fetchMoviesHandler() {
+   const response = await fetch('https://swapi.dev/api/films/');
+   const data = await response.json();
+  
       const transformedMovies = data.results.map(movieData => {
         return {
           id: movieData.episode_id,
@@ -21,8 +20,8 @@ function App() {
         }
       });
       setMovies(transformedMovies);
-    });
-  }
+    };
+  
 
   return (
     <React.Fragment>
@@ -34,6 +33,6 @@ function App() {
       </section>
     </React.Fragment>
   );
-}
+  }
 
 export default App;
