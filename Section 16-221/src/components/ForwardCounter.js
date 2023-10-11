977eab5093d4ad0d481be372de6e9516aafee7a8
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-10-11 14:02:13 +0530

    Creating a Custom React Hook Function

---
+++ Section 16-221/src/components/ForwardCounter.js
@@ -0,0 +1,11 @@
+import Card from './Card';
+import useCounter from '../hooks/use-counter';
+
+const ForwardCounter = () => {
+
+  const counter  = useCounter();
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
