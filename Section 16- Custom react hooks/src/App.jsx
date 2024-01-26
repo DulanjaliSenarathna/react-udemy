3cdb0e6c96a0961dffc5f63b89aec80ddf98160c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-01-26 10:11:14 +0530

    Exposing Nested Functions From The Custom Hook

--- Section 16- Custom react hooks/src/App.jsx
+++ Section 16- Custom react hooks/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
- const {isFetching,error,fetchedData}= useFetch(fetchUserPlaces,[]);
+ const {isFetching,error,fetchedData:userPlaces,setFetchedData:setUserPlaces}= useFetch(fetchUserPlaces,[]);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -29,52 +29,52 @@ function App() {
     setModalIsOpen(false);
   }
 
-  // async function handleSelectPlace(selectedPlace) {
-  //   // await updateUserPlaces([selectedPlace, ...userPlaces]);
+  async function handleSelectPlace(selectedPlace) {
+    await updateUserPlaces([selectedPlace, ...userPlaces]);
 
-  //   // setUserPlaces((prevPickedPlaces) => {
-  //   //   if (!prevPickedPlaces) {
-  //   //     prevPickedPlaces = [];
-  //   //   }
-  //   //   if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
-  //   //     return prevPickedPlaces;
-  //   //   }
-  //   //   return [selectedPlace, ...prevPickedPlaces];
-  //   // });
+    setUserPlaces((prevPickedPlaces) => {
+      if (!prevPickedPlaces) {
+        prevPickedPlaces = [];
+      }
+      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
+        return prevPickedPlaces;
+      }
+      return [selectedPlace, ...prevPickedPlaces];
+    });
 
-  //   try {
-  //     await updateUserPlaces([selectedPlace, ...userPlaces]);
-  //   } catch (error) {
-  //     setUserPlaces(userPlaces);
-  //     setErrorUpdatingPlaces({
-  //       message: error.message || 'Failed to update places.',
-  //     });
-  //   }
-  // }
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || 'Failed to update places.',
+      });
+    }
+  }
 
-  // const handleRemovePlace = useCallback(
-  //   async function handleRemovePlace() {
-  //     setUserPlaces((prevPickedPlaces) =>
-  //       prevPickedPlaces.filter(
-  //         (place) => place.id !== selectedPlace.current.id
-  //       )
-  //     );
+  const handleRemovePlace = useCallback(
+    async function handleRemovePlace() {
+      setUserPlaces((prevPickedPlaces) =>
+        prevPickedPlaces.filter(
+          (place) => place.id !== selectedPlace.current.id
+        )
+      );
 
-  //     try {
-  //       await updateUserPlaces(
-  //         userPlaces.filter((place) => place.id !== selectedPlace.current.id)
-  //       );
-  //     } catch (error) {
-  //       setUserPlaces(userPlaces);
-  //       setErrorUpdatingPlaces({
-  //         message: error.message || 'Failed to delete place.',
-  //       });
-  //     }
+      try {
+        await updateUserPlaces(
+          userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+        );
+      } catch (error) {
+        setUserPlaces(userPlaces);
+        setErrorUpdatingPlaces({
+          message: error.message || 'Failed to delete place.',
+        });
+      }
 
-  //     setModalIsOpen(false);
-  //   },
-  //   [userPlaces]
-  // );
+      setModalIsOpen(false);
+    },
+    [userPlaces,setUserPlaces]
+  );
 
   function handleError() {
     setErrorUpdatingPlaces(null);
@@ -95,7 +95,7 @@ function App() {
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
-         // onConfirm={handleRemovePlace}
+         onConfirm={handleRemovePlace}
         />
       </Modal>
 
@@ -115,13 +115,13 @@ function App() {
             fallbackText="Select the places you would like to visit below."
             isLoading={isFetching}
             loadingText="Fetching your places..."
-            places={fetchedData}
+            places={userPlaces}
             onSelectPlace={handleStartRemovePlace}
           />
         )}
 
         <AvailablePlaces 
-       // onSelectPlace={handleSelectPlace} 
+        onSelectPlace={handleSelectPlace} 
         />
       </main>
     </>
--- Section 16- Custom react hooks/src/hooks/useFetch.jsx
+++ Section 16- Custom react hooks/src/hooks/useFetch.jsx
@@ -25,6 +25,7 @@ export function useFetch(fetchFn,initialValue){
       return {
         isFetching,
         error,
+        setFetchedData,
         fetchedData
       }
 }
