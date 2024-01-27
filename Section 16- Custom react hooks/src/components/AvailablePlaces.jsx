55166fee5f52fdd3149717953d51e37e5dbe4ef3
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-01-27 21:36:57 +0530

    Creating Flexible Custom Hooks

--- Section 16- Custom react hooks/src/components/AvailablePlaces.jsx
+++ Section 16- Custom react hooks/src/components/AvailablePlaces.jsx
@@ -6,20 +6,30 @@ import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 import { useFetch } from '../hooks/useFetch.jsx';
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
+async function fetchSortedPlaces(){
+  const places = await fetchAvailablePlaces();
+  return new Promise((resolve)=>{
+    navigator.geolocation.getCurrentPosition((position) => {
+
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+      
+    });
+  });
+ 
+}
+
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
   
 
- const {isFetching,error,fetchedData:availablePlaces, setFetchedData:setAvailablePlaces}= useFetch(fetchAvailablePlaces,[]);
+ const {isFetching,error,fetchedData:availablePlaces,}= useFetch(fetchSortedPlaces,[]);
 
 
   if (error) {
