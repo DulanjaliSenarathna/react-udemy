22196f031ad155b9fa95b7643edbe9a982e4e7f2
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-02-06 13:00:47 +0530

    Getting Values via FormData & Native Browser APIs

--- Section 17- Forms user inputs/src/App.jsx
+++ Section 17- Forms user inputs/src/App.jsx
@@ -1,12 +1,12 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
