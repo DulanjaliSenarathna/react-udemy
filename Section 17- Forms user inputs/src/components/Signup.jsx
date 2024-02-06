1d41f2d61b6cfa29543ed9fbb14c700e212b9ece
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-02-06 13:22:58 +0530

    Resetting Forms

--- Section 17- Forms user inputs/src/components/Signup.jsx
+++ Section 17- Forms user inputs/src/components/Signup.jsx
@@ -9,6 +9,7 @@ export default function Signup() {
   const data = Object.fromEntries(fd.entries());
   data.acquisition = acquisitionChannel;
   console.log(data);
+  event.target.reset();
   }
   return (
     <form onSubmit={handleSubmit}>
--- Section 17- Forms user inputs/src/components/StateLogin.jsx
+++ Section 17- Forms user inputs/src/components/StateLogin.jsx
@@ -13,6 +13,10 @@ export default function Login() {
   function handleSubmit(event){
     event.preventDefault();
     console.log(enteredValues);
+    setEnteredValues({
+      email:'',
+      password:''
+    })
   }
 
   function handleInputChange(identifier,value){
