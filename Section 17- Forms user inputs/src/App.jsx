fcc8511ead9d5a547f99c4698dd339925e4faf22
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-02-08 23:56:08 +0530

    Building & Using a Reusable Input Component

--- Section 17- Forms user inputs/src/App.jsx
+++ Section 17- Forms user inputs/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+
 import Signup from './components/Signup';
+import Login from './components/StateLogin';
 
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <>
       <Header />
       <main>
+        <Login/>
         
-        <Signup/>
       </main>
     </>
   );
--- Section 17- Forms user inputs/src/components/StateLogin.jsx
+++ Section 17- Forms user inputs/src/components/StateLogin.jsx
@@ -1,4 +1,5 @@
 import {  useState } from "react";
+import Input from "./Input";
 
 export default function Login() {
 
@@ -16,6 +17,7 @@ export default function Login() {
   });
 
   const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const passwordIsInvalid = didEdit.password && !enteredValues.password.trim().length<6;
 
   function handleSubmit(event){
     event.preventDefault();
@@ -56,19 +58,19 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" 
-          onBlur={()=>handleInputBlur('email')}
-          onChange={(event)=>handleInputChange('email',event.target.value)} value={enteredValues.email}/>
-          <div className="control-error">{emailIsInvalid && <p>Please entered valid email</p>}</div>
-        </div>
+        <Input label="Email" id="email" type="email" name="email" 
+        onBlur={()=>handleInputBlur('email')}
+        onChange={(event)=>handleInputChange('email',event.target.value)} value={enteredValues.email}
+        error={emailIsInvalid && 'Please enter a valid email'}
+        />
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" 
-          onChange={(event)=>handleInputChange('password',event.target.value)} value={enteredValues.password}/>
-        </div>
+      <Input label="Password" id="password" type="password" name="password" 
+        onBlur={()=>handleInputBlur('password')}
+        onChange={(event)=>handleInputChange('password',event.target.value)} value={enteredValues.password}
+        error={passwordIsInvalid && 'Please enter a valid password'}
+        />
+
+        
       </div>
 
       <p className="form-actions">
