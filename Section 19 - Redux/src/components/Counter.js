6f76037aa1bc66af3d2dd9d41d8d44fd61b5fd46
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-02-29 21:18:08 +0530

    Working with Multiple State Properties

--- Section 19 - Redux/src/components/Counter.js
+++ Section 19 - Redux/src/components/Counter.js
@@ -7,6 +7,7 @@ const Counter = () => {
 
   const dispatch = useDispatch();
  const counter = useSelector(state=>state.counter);
+ const toggle = useSelector(state=>state.showCounter)
 
  const incrementHandler = () =>{
   dispatch({type: 'increment'})
@@ -18,12 +19,14 @@ const Counter = () => {
  const decrementHandler = () =>{
   dispatch({type: 'decrement'})
  };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { toggle && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increseHandler}>Increse by 5</button>
--- Section 19 - Redux/src/store/index.js
+++ Section 19 - Redux/src/store/index.js
@@ -1,20 +1,30 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter : true }, action) => {
     if (action.type === 'increment') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
     if (action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'increse') {
         return {
-            counter: state.counter + action.amount
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
+    }
+
+    if(action.type === 'toggle'){
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
