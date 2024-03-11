7378885579251f48e9915f501871f117e1b99a76
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-03-11 15:24:03 +0530

    Refresher 2 done

--- Section 20 - Redux and Side Effects/src/store/cart-slice.js
+++ Section 20 - Redux and Side Effects/src/store/cart-slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
-    name:cart,
+const cartSlice = createSlice({
+    name:'cart',
     initialState:{
         items:[],
         totalQuantity: 0,
@@ -10,9 +10,10 @@ createSlice({
         addItemToCart (state,action){
             const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
+            state.totalQuantity++;
             if(!existingItem){
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity:1,
                     totalPrice: newItem.price,
@@ -20,10 +21,25 @@ createSlice({
                 });
 
             }else{
-                existingItem.quantity++,
-               existingItem.totalPrice = existingItem.totalPrice + newItem.price
+                existingItem.quantity++;
+               existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
-        removeItemFromCart (){}
+        removeItemFromCart (state, action){
+            state.totalQuantity--;
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            if(existingItem.quantity ===1){
+                state.items = state.items.filter((item)=>item.id !== id);
+            }else{
+                existingItem.quantity--;
+               // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+
+        }
     }
 })
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
--- Section 20 - Redux and Side Effects/src/store/index.js
+++ Section 20 - Redux and Side Effects/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
+
 
 const store = configureStore({
-    reducer: {ui: uiSlice.reducer}
+    reducer: {ui: uiSlice.reducer, cart: cartSlice.reducer}
 })
 
 export default store;
