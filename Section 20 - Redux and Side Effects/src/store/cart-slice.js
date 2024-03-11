d5ac514cc56c2c0b1917b7f3ce2de4c97ba27ae1
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-03-11 08:46:47 +0530

    Refresher practise part 1 done

---
+++ Section 20 - Redux and Side Effects/src/store/cart-slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+    name:cart,
+    initialState:{
+        items:[],
+        totalQuantity: 0,
+    },
+    reducers:{
+        addItemToCart (state,action){
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            if(!existingItem){
+                state.items.push({
+                    itemId: newItem.id,
+                    price: newItem.price,
+                    quantity:1,
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                });
+
+            }else{
+                existingItem.quantity++,
+               existingItem.totalPrice = existingItem.totalPrice + newItem.price
+            }
+        },
+        removeItemFromCart (){}
+    }
+})
