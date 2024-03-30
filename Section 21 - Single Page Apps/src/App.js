f3fec014f1626d2008bf47c66528ec3b41372eae
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-03-30 12:22:09 +0530

    Adding a Second Route

--- Section 21 - Single Page Apps/src/App.js
+++ Section 21 - Single Page Apps/src/App.js
@@ -1,9 +1,10 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/Home';
+import Products from './pages/Products';
 
 const router = createBrowserRouter([
   {path:'/', element: <HomePage/>},
-  {}
+  {path:'/products', element: <Products/>},
 ])
 
 function App() {
---
+++ Section 21 - Single Page Apps/src/pages/Products.js
@@ -0,0 +1,5 @@
+function Products(){
+    return <h1>Products</h1>
+}
+
+export default Products
