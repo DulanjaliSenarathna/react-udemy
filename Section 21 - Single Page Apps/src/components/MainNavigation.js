752aa873efad4a97717dde34724f8b9e12c244ba
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-04-02 17:07:56 +0530

    Working with Navigation Links (NavLink)

--- Section 21 - Single Page Apps/src/components/MainNavigation.js
+++ Section 21 - Single Page Apps/src/components/MainNavigation.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from './MainNavigation.module.css'
 
 function MainNavigation (){
 return <header className={classes.header}>
     <nav>
         <ul className={classes.list}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/products">Products</Link></li>
+            <li><NavLink to="/" className={({isActive})=>isActive? classes.active : undefined} end
+            //style={({isActive})=> ({textAlign: isActive? 'center': 'left'})}
+            >Home</NavLink></li>
+            <li><NavLink to="/products" className={({isActive})=>isActive? classes.active : undefined}>Products</NavLink></li>
         </ul>
     </nav>
 </header>
