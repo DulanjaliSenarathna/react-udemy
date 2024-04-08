93bbadc0c29b6eb97aff63064facffed6a5b7893
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-04-08 11:11:46 +0530

    Using Data From A Loader In The Route Component

--- Section 21-1 SPA 2nd Project/frontend/src/pages/Events.js
+++ Section 21-1 SPA 2nd Project/frontend/src/pages/Events.js
@@ -1,28 +1,14 @@
-import { useEffect, useState } from 'react';
+import {useLoaderData} from 'react-router-dom'
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
+  const events = useLoaderData();
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      
+       <EventsList events={events} />
     </>
   );
 }
