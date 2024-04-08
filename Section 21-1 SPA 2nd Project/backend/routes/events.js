154519d670256f34a79e2671890654f8160e00b4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-04-08 14:31:38 +0530

    Reflecting The Current Navigation State in the UI

--- Section 21-1 SPA 2nd Project/backend/routes/events.js
+++ Section 21-1 SPA 2nd Project/backend/routes/events.js
@@ -12,7 +12,10 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const events = await getAll();
-    res.json({ events: events });
+    
+      res.json({ events: events });
+    
+   
   } catch (error) {
     next(error);
   }
