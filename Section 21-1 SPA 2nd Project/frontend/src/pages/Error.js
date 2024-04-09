8a3cd5c989a2b7f64f17c5570ad66c764e1eca3d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-04-09 09:24:02 +0530

    The json() Utility Function

--- Section 21-1 SPA 2nd Project/frontend/src/pages/Error.js
+++ Section 21-1 SPA 2nd Project/frontend/src/pages/Error.js
@@ -10,7 +10,7 @@ function ErrorPage(){
     let message = 'Something went wrong';
 
     if(error.status ===500){
-        message = JSON.parse(error.data).message;
+        message = error.data.message;
     }
 
     if(error.status ===404){
