c231368f6852e09bd6363b25ea4cdd30976da895
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-02 20:46:53 +0530

    Installing & Using Tanstack Query - And Seeing Why It's Great!

--- Section 24 - Tanstack Query/src/App.jsx
+++ Section 24 - Tanstack Query/src/App.jsx
@@ -8,6 +8,7 @@ import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -37,8 +38,10 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return <QueryClientProvider client={queryClient}><RouterProvider router={router} /></QueryClientProvider> ;
 }
 
 export default App;
