012140f43bda1273b09974fe4b10b955edd98ff8
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-02 09:11:45 +0530

    Attaching Auth Tokens to Outgoing Requests

--- Section 22- Authentication/frontend/src/components/EventForm.js
+++ Section 22- Authentication/frontend/src/components/EventForm.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
+import { getAuthToken } from '../util/auth';
 
 function EventForm({ method, event }) {
   const data = useActionData();
@@ -86,6 +87,7 @@ export default EventForm;
 export async function action({ request, params }) {
   const method = request.method;
   const data = await request.formData();
+  const token = getAuthToken();
 
   const eventData = {
     title: data.get('title'),
@@ -105,7 +107,11 @@ export async function action({ request, params }) {
     method: method,
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
     },
+
+     
+    
     body: JSON.stringify(eventData),
   });
 
--- Section 22- Authentication/frontend/src/pages/Authentication.js
+++ Section 22- Authentication/frontend/src/pages/Authentication.js
@@ -38,5 +38,10 @@ if(mode !== 'login' && mode !== 'signup' ){
   throw json({message:'Could not authenticate user.'}, {status:500})
  }
 
+ const resData = await response.json();
+ const token = resData.token;
+
+ localStorage.setItem('token', token);
+
  return redirect('/');
 }
