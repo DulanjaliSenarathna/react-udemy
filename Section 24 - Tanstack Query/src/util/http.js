aa040b676e25def7f50d7cf4f3c9f03b9816653d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-03 11:18:03 +0530

    Dynamic Query Functions & Query Keys

--- Section 24 - Tanstack Query/src/util/http.js
+++ Section 24 - Tanstack Query/src/util/http.js
@@ -1,6 +1,12 @@
-export async function fetchEvents() {
+export async function fetchEvents(searchTerm) {
+
+    const url = 'http://localhost:3000/events'
+
+    if(searchTerm){
+        url += '?search=' + searchTerm;
+    }
     
-    const response = await fetch('http://localhost:3000/events');
+    const response = await fetch(url);
 
     if (!response.ok) {
       const error = new Error('An error occurred while fetching the events');
