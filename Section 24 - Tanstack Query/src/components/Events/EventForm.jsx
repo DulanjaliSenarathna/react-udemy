80e129fb464ced0fa8e13843d43d98f1bf6c735c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-05 08:40:09 +0530

    Fetching More Data & Testing the Mutation

--- Section 24 - Tanstack Query/src/components/Events/EventForm.jsx
+++ Section 24 - Tanstack Query/src/components/Events/EventForm.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 
 import ImagePicker from '../ImagePicker.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchSelectableImages } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+  const {data, isPending, isError} = useQuery({
+    queryKey: ['events-images'],
+    queryFn: fetchSelectableImages
+  });
 
   function handleSelectImage(image) {
     setSelectedImage(image);
@@ -30,13 +38,16 @@ export default function EventForm({ inputData, onSubmit, children }) {
         />
       </p>
 
-      <div className="control">
+      {isPending && <p>Loading...</p>}
+      {isError && <ErrorBlock title='Fsisled to load selectable images' message='Please try again later'/>}
+
+      {data && <div className="control">
         <ImagePicker
-          images={[]}
+          images={data}
           onSelect={handleSelectImage}
           selectedImage={selectedImage}
         />
-      </div>
+      </div>}
 
       <p className="control">
         <label htmlFor="description">Description</label>
