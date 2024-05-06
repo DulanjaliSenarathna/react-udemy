49f3564c81dfae1c684a631403eeb89babf382ad
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-06 13:18:05 +0530

    React Query Advantages In Action

--- Section 24 - Tanstack Query/src/components/Events/EditEvent.jsx
+++ Section 24 - Tanstack Query/src/components/Events/EditEvent.jsx
@@ -1,10 +1,20 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
+import LoadingIndicator from './../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+ const params = useParams();
+
+  const {data, isPending, isError, error} = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal})=> fetchEvent({signal, id:params.id})
+  });
 
   function handleSubmit(formData) {}
 
@@ -12,16 +22,35 @@ export default function EditEvent() {
     navigate('../');
   }
 
+  let content;
+
+  if(isPending){
+    content = <div className='center'><LoadingIndicator/></div>
+  }
+
+  if(isError){
+    content = <>
+    <ErrorBlock title='Failed to load event' message={error.info?.message || 'Failed to load event. Please check your inputs and try again later'}/>
+    <div className='form-actions'>
+      <Link to='../' className='button'>OK</Link>
+    </div>
+    </>
+  }
+
+  if(data){
+    content= <EventForm inputData={data} onSubmit={handleSubmit}>
+    <Link to="../" className="button-text">
+      Cancel
+    </Link>
+    <button type="submit" className="button">
+      Update
+    </button>
+  </EventForm>
+  }
+
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
