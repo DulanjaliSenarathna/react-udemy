005b253ed30939fe72e4c6520b74fe86301e9a80
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-09 18:01:40 +0530

    Populating The Starting Page Content

--- Section 25 -2 Foof App Next JS/app/page.js
+++ Section 25 -2 Foof App Next JS/app/page.js
@@ -1,14 +1,51 @@
 import Link from "next/link";
+import classes from './page.module.css'
 
 export default function Home() {
   return (
+    <>
+    <header className={classes.header}>
+      <div className={classes.slidshow}>
+
+      </div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Foods for NextLevel Foodies</h1>
+          <p>Taste & share food from all over the world</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href='/community'>Join the community</Link>
+          <Link href='/meals'>Explore Meals</Link>
+        </div>
+      </div>
+    </header>
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <Link href='/meals'>Meals</Link>
-      <Link href='/meals/share'>Share Meals</Link>
-      <Link href='/community'>Community</Link>
+    <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
     </main>
+    </>
   );
 }
