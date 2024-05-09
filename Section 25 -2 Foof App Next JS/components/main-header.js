8f987bd7e46e85a44d270de4bdf0006df6d05a22
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-09 15:29:54 +0530

    Styling NextJS Project: Your Options & Using CSS Modules

--- Section 25 -2 Foof App Next JS/components/main-header.js
+++ Section 25 -2 Foof App Next JS/components/main-header.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import logoImg from '@/assets/logo.png'
+import classes from './main-header.module.css'
 
 export default function MainHeader(){
-    return <header>
-        <Link href='/'>
+    return <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
             <img src={logoImg.src}/>
             Next Level Foods
         </Link>
-        <nav>
+        <nav className={classes.nav}>
             <ul>
                 <li>
                     <Link href='/meals'>Browse Meals</Link>
