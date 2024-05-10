777cdfaf641172c5454b857da6944c2903faa62f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-10 16:55:01 +0530

    Using Client Components Efficiently

--- Section 25 -2 Foof App Next JS/components/main-header/main-header.js
+++ Section 25 -2 Foof App Next JS/components/main-header/main-header.js
@@ -1,10 +1,14 @@
+
 import Link from "next/link";
 import logoImg from '../../assets/logo.png'
 import classes from '../main-header/main-header.module.css'
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader(){
+
+   
     return <>
     
     <MainHeaderBackground/>
@@ -16,11 +20,11 @@ export default function MainHeader(){
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href='/meals'>Browse Meals</Link>
-                </li>
+                    <NavLink href='/meals'>Browse Meals</NavLink>
+                   </li>
                 <li>
-                    <Link href='/community'>Foodie Community</Link>
-                </li>
+                <NavLink href='/community'>Community</NavLink>  
+                            </li>
             </ul>
         </nav>
     </header>
