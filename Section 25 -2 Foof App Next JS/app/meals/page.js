bdde252507f73a25b34401f58c0141816a289e8d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-10 18:43:29 +0530

    Outputting Meals Data & Images With Unknown Dimensions

--- Section 25 -2 Foof App Next JS/app/meals/page.js
+++ Section 25 -2 Foof App Next JS/app/meals/page.js
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+
 export default function MealsPage(){
-    return <h1>Meals Page</h1>
+    return <>
+    <header className={classes.header}>
+        <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favourite recipe and cook it by youeself. It is easy and fun.</p>
+        <p className={classes.cta}>
+            <Link href='/meals/share'>Share Your Favourite Recipe</Link>
+        </p>
+    </header>
+    <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+    </main>
+    </>
 }
