4e3cc8215d20f02cd75c85fa5650fe10e2b93af4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-11 12:17:49 +0530

    Handling "Not Found" States

---
+++ Section 25 -2 Foof App Next JS/app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return (<main className="not-found">
+        <h1>Not Found</h1>
+        <p>Unfortunately, we could not find the requested page or resource</p>
+    </main>)
+
+}
--- Section 25 -2 Foof App Next JS/lib/meals.js
+++ Section 25 -2 Foof App Next JS/lib/meals.js
@@ -5,6 +5,6 @@ const db = sql('meals.db');
 export async function getMeals(){
     await new Promise((resolve)=> setTimeout(resolve,5000));
 
-    throw new Error('Loading Meals failed');
+    //throw new Error('Loading Meals failed');
    return db.prepare('SELECT * FROM meals').all();
 }
