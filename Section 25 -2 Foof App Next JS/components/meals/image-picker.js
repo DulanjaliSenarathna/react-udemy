ad7e44c56e0a3e26ff884e3428ec6ae2afe06e60
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-11 19:07:10 +0530

    Adding an Image Preview to the Picker

--- Section 25 -2 Foof App Next JS/components/meals/image-picker.js
+++ Section 25 -2 Foof App Next JS/components/meals/image-picker.js
@@ -1,18 +1,43 @@
 'use client'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import classes from './image-picker.module.css'
+import Image from 'next/image';
 
 export default function ImagePicker({label, name}){
+
+   const [pickedImage, setPickedImage] = useState();
 
    const imageInput = useRef();
 
  function handlePickClick(){
     imageInput.current.click();
     }
+
+function handleImageChange(event){
+    const file = event.target.files[0];
+
+    if(!file){
+        setPickedImage(null);
+        return;
+       
+    }
+
+   const fileReader = new FileReader();
+
+   fileReader.onload = () =>{
+    setPickedImage(fileReader.result);
+   }
+   fileReader.readAsDataURL(file);
+    }
     return <div className={classes.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
-            <input className={classes.input} type='file' id={name} accept='image/png, image/jpeg' name={name} ref={imageInput} /> 
+            <div className={classes.preview}>
+                {!pickedImage && <p>No image picked yet</p>}
+                {pickedImage && <Image src={pickedImage} alt='the image selected the user' fill/>}
+            </div>
+            <input className={classes.input} type='file' id={name} accept='image/png, image/jpeg'
+             name={name} ref={imageInput} onChange={handleImageChange} /> 
             <button className={classes.button} type="button" onClick={handlePickClick}>
                 Pick an Image
             </button>
