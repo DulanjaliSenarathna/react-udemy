f77d996c16b81fe537d42f5bf06a7a4ecacfc252
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-11 12:06:40 +0530

    Handling Errors

---
+++ Section 25 -2 Foof App Next JS/app/meals/error.js
@@ -0,0 +1,7 @@
+'use client'
+export default function Error(){
+    return <main className="error">
+        <h1>An error occured</h1>
+        <p>Failed to fetch meal data. Please try again later</p>
+    </main>
+}
--- Section 25 -2 Foof App Next JS/lib/meals.js
+++ Section 25 -2 Foof App Next JS/lib/meals.js
@@ -3,6 +3,8 @@ import sql from 'better-sqlite3'
 const db = sql('meals.db');
 
 export async function getMeals(){
-    await new Promise((resolve)=> setTimeout(resolve,5000))
+    await new Promise((resolve)=> setTimeout(resolve,5000));
+
+    throw new Error('Loading Meals failed');
    return db.prepare('SELECT * FROM meals').all();
 }
