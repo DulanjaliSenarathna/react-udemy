e6d4f77ba1fdbeb6500306ddda50786e030cfb17
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-11 13:06:32 +0530

    Throwing Not Found Errors For Individual Meals

--- Section 25 -2 Foof App Next JS/app/meals/[mealSlug]/page.js
+++ Section 25 -2 Foof App Next JS/app/meals/[mealSlug]/page.js
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
+import { notFound } from 'next/navigation';
 
 export default function MealDetailPage({params}){
 
    const meal =  getMeal(params.mealSlug);
+
+   if(!meal){
+    notFound();
+   }
    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
     return <>
     <header className={classes.header}>
---
+++ Section 25 -2 Foof App Next JS/app/meals/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return (<main className="not-found">
+        <h1>Meal Not Found</h1>
+        <p>Unfortunately, we could not find the requested page or meal data</p>
+    </main>)
+
+}
