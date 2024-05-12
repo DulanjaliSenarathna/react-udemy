a7740e9e3a65024cfb689e3dc2530f8907db56d9
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-12 11:38:04 +0530

    Storing Server Actions in Separate Files

--- Section 25 -2 Foof App Next JS/app/meals/share/page.js
+++ Section 25 -2 Foof App Next JS/app/meals/share/page.js
@@ -1,21 +1,8 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-
- async function shareMeal(formData){
-    'use server'
-
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    }
-
-    console.log(meal);
   }
   return (
     <>
@@ -62,4 +49,3 @@ export default function ShareMealPage() {
       </main>
     </>
   );
-}
