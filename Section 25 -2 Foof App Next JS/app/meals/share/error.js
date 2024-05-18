5d7575c9af43ab445a2e5e88f3f70bf208683aac
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-18 12:48:07 +0530

    Adding Server-Side Input Validation

---
+++ Section 25 -2 Foof App Next JS/app/meals/share/error.js
@@ -0,0 +1,7 @@
+'use client'
+export default function Error(){
+    return <main className="error">
+        <h1>An error occured</h1>
+        <p>Failed to create meal.</p>
+    </main>
+}
--- Section 25 -2 Foof App Next JS/lib/actions.js
+++ Section 25 -2 Foof App Next JS/lib/actions.js
@@ -3,6 +3,10 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isInvalidText(text){
+  return !text || text.trim() === '';
+}
+
 export async function shareMeal(formData){
     
 
@@ -14,6 +18,12 @@ export async function shareMeal(formData){
       creator: formData.get('name'),
       creator_email: formData.get('email')
     };
+
+    if(isInvalidText(meal.title) || isInvalidText(meal.summary) || isInvalidText(meal.instructions)
+       || isInvalidText(meal.creator) || isInvalidText(meal.title) 
+      || isInvalidText(meal.creator_email) || !meal.creator_email.includes('@') || !meal.image || meal.image.size === 0){
+        throw new Error('Invalid Input');
+      }
  await saveMeal(meal);
  redirect('/meals')
   }
