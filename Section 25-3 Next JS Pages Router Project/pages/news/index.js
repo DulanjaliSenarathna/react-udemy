a763c40ecbe46972f1653f0c05236ce09241a041
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-19 17:24:42 +0530

    Linking Between Pages

--- Section 25-3 Next JS Pages Router Project/pages/news/index.js
+++ Section 25-3 Next JS Pages Router Project/pages/news/index.js
@@ -1,5 +1,14 @@
+import { Fragment } from "react";
+import { Link } from 'next/link';
+
 function NewsPage(){
-    return <h1>The News Page</h1>
+    return <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li><Link href="/news/nextjs-is-a-great-framework">Next JS is a great framework</Link></li>
+            <li>Something else</li>
+        </ul>
+    </Fragment>
 }
 
 export default NewsPage;
