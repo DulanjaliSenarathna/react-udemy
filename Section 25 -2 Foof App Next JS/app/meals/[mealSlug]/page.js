0a5f2684187e98f46e54404bfaa98896c9a36e43
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-19 11:39:09 +0530

    Adding Dynamic Metadata

--- Section 25 -2 Foof App Next JS/app/meals/[mealSlug]/page.js
+++ Section 25 -2 Foof App Next JS/app/meals/[mealSlug]/page.js
@@ -3,6 +3,18 @@ import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({params}){
+    const meal =  getMeal(params.mealSlug);
+    
+    if(!meal){
+        notFound();
+    }
+    return {
+        title: meal.title,
+        description: meal.summary
+    };
+}
+
 export default function MealDetailPage({params}){
 
    const meal =  getMeal(params.mealSlug);
