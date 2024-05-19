3a2647ddd557e4c23fa5980f9957e443ac399c63
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-19 10:44:31 +0530

    Adding Static Metadata

--- Section 25 -2 Foof App Next JS/app/meals/page.js
+++ Section 25 -2 Foof App Next JS/app/meals/page.js
@@ -4,6 +4,11 @@ import MealsGrid from '@/components/meals/meals-grid'
 import { getMeals } from '@/lib/meals'
 import { Suspense } from 'react';
 
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious meals, shared by our vibrant community.',
+  };
+
 async function Meals(){
     const meals = await getMeals();
     return <MealsGrid meals={meals}/>
