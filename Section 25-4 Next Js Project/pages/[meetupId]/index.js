58cb3fc85c8ccf7f44b58be13b6bad2c5439433a
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-20 14:24:10 +0530

    Adding Custom Components & CSS Modules

---
+++ Section 25-4 Next Js Project/pages/[meetupId]/index.js
@@ -0,0 +1,8 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+    return  <MeetupDetail image="https://www.eventbrite.com/blog/wp-content/uploads/2023/02/Frame-1-5-min-1-1536x864.avif" title='First Meetup' address='776/A, Rukhena Watta, Kanuggalla,Ellawala' description='Before you start planning your event, you need to make a list of your trackable goals'/>
+}
+
+export default MeetupDetails;
