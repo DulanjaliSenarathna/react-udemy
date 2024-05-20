64270044621cfec7e0a9b47aa93e1322e3a31b42
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-20 19:54:16 +0530

    Working with Params for SSG Data Fetching

--- Section 25-4 Next Js Project/pages/[meetupId]/index.js
+++ Section 25-4 Next Js Project/pages/[meetupId]/index.js
@@ -2,7 +2,28 @@ import { Fragment } from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails(){
-    return  <MeetupDetail image="https://www.eventbrite.com/blog/wp-content/uploads/2023/02/Frame-1-5-min-1-1536x864.avif" title='First Meetup' address='776/A, Rukhena Watta, Kanuggalla,Ellawala' description='Before you start planning your event, you need to make a list of your trackable goals'/>
+    return  <MeetupDetail 
+    image="https://www.eventbrite.com/blog/wp-content/uploads/2023/02/Frame-1-5-min-1-1536x864.avif" 
+    title='First Meetup'
+    address='776/A, Rukhena Watta, Kanuggalla,Ellawala'
+    description='Before you start planning your event, you need to make a list of your trackable goals'/>
 }
 
+export async function getStaticProps(context){
+    // fetch data for single meetup
+
+    const meetupId = context.params.meetupId;
+
+    return {
+        props:{
+            meetupData:{
+                id: meetupId,
+                image: "https://www.eventbrite.com/blog/wp-content/uploads/2023/02/Frame-1-5-min-1-1536x864.avif",
+                title: 'First Meetup',
+                address: '776/A, Rukhena Watta, Kanuggalla,Ellawala',
+                description: 'Before you start planning your event, you need to make a list of your trackable goals'
+            }
+        }
+    }
+}
 export default MeetupDetails;
