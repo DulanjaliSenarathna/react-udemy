6b9ca79d50ed080c88087de93cb959c0a1faa3fb
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-20 21:04:13 +0530

    Introducing API Routes

---
+++ Section 25-4 Next Js Project/pages/api/new-meetup.js
@@ -0,0 +1,12 @@
+// api/new-meetup
+
+function handler(req, res){
+if(req.method === 'POST'){
+    const data = req.body;
+
+    const {title,image,address,description} = data;
+}
+}
+
+export default handler;
+
