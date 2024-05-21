e7fb384519b8b337f972802130d60d1813bde840
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-21 16:03:49 +0530

    Animating Between Conditional Values

--- Section 26 - Animating React Apps/src/components/ChallengeItem.jsx
+++ Section 26 - Animating React Apps/src/components/ChallengeItem.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
+import { motion } from 'framer-motion';
 
 export default function ChallengeItem({
   challenge,
@@ -42,11 +43,13 @@ export default function ChallengeItem({
             </p>
           </div>
         </header>
-        <div className={`challenge-item-details ${isExpanded ? 'expanded' : ''}`}>
+        <div className={"challenge-item-details"}>
           <p>
             <button onClick={onViewDetails}>
               View Details{' '}
-              <span className="challenge-item-details-icon">&#9650;</span>
+              <motion.span className="challenge-item-details-icon" animate={{rotate: isExpanded ? 180 : 0}}>
+                &#9650;
+              </motion.span>
             </button>
           </p>
 
