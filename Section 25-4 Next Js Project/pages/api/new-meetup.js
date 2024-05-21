3eb48eda24f2416ea01c49fb1fc0e66f72c4074b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-21 08:23:10 +0530

    Working with MongoDB

--- Section 25-4 Next Js Project/pages/api/new-meetup.js
+++ Section 25-4 Next Js Project/pages/api/new-meetup.js
@@ -1,10 +1,18 @@
 // api/new-meetup
+import {MongoClient} from 'mongodb'
 
-function handler(req, res){
+async function handler(req, res){
 if(req.method === 'POST'){
     const data = req.body;
 
-    const {title,image,address,description} = data;
+  const client = await MongoClient.connect('mongodb+srv://Dula_123:uBXBt07221EwcG06@cluster0.iw8arkr.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+
+  const meetupCollection = db.collection('meetups');
+const result = await meetupCollection.insertOne(data);
+console.log(result);
+client.close();
+res.status(201).json({message:'Meetup Inserted!'})
 }
 }
 
