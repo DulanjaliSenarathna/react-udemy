948b3b9298b9486eb39103c5f8ecc7bf783b895d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-21 19:09:08 +0530

    Reusing Animation States

--- Section 26 - Animating React Apps/src/components/Modal.jsx
+++ Section 26 - Animating React Apps/src/components/Modal.jsx
@@ -2,13 +2,20 @@ import { createPortal } from 'react-dom';
 import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
+
+  // const hiddenAnimationState = {opacity:0 , y:30};
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog open className="modal" 
-      initial={{opacity:0 , y:30}} 
-      animate={{opacity:1 , y:0}}
-      exit={{opacity:0 , y:30}}
+      variants={{
+        hidden: {opacity:0 , y:30},
+        visible: {opacity:1 , y:0}
+      }}
+      initial= "hidden"
+      animate="visible"
+      exit= "hidden"
+      whileHover= "hidden"
       >
         <h2>{title}</h2>
         {children}
