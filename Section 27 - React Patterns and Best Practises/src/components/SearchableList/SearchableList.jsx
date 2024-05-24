1f810075ba59595eb1429c663849ce2e02055e36
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-24 11:49:00 +0530

    Working with Debouncing

--- Section 27 - React Patterns and Best Practises/src/components/SearchableList/SearchableList.jsx
+++ Section 27 - React Patterns and Best Practises/src/components/SearchableList/SearchableList.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export default function SearchableList({items,itemKeyFn, children}){
+
+    const lastChange = useRef();
 
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults = items.filter(item=> JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()))
 
     function handleChange(event){
-        setSearchTerm(event.target.value);
+
+        if(lastChange.current){
+            clearTimeout(lastChange.current);
+        }
+
+       lastChange.current = setTimeout(()=>{
+        lastChange.current = null;
+            setSearchTerm(event.target.value);
+        },500);
+        
     }
 return <div className="searchable-list">
 <input type="search" placeholder="search" onChange={handleChange}/>
