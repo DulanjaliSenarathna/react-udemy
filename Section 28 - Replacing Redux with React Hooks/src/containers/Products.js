612dca2db3004487103019fe82972aeee04ba2a0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-24 15:40:16 +0530

    Alternative: Using the Context API

--- Section 28 - Replacing Redux with React Hooks/src/containers/Products.js
+++ Section 28 - Replacing Redux with React Hooks/src/containers/Products.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React ,{useContext} from 'react';
 import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
+import { ProductsContext } from '../context/products-context';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  const productList = useContext(ProductsContext).products;
+  
   return (
     <ul className="products-list">
       {productList.map(prod => (
--- Section 28 - Replacing Redux with React Hooks/src/index.js
+++ Section 28 - Replacing Redux with React Hooks/src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+
+
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products-context'
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
 
-const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
+    <ProductsProvider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </Provider>);
+    </ProductsProvider>);
