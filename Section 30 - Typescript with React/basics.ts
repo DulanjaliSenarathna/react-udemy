4ac74ec00ea8cd92670cecf61f4f3d1fa5b663ac
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-25 18:42:55 +0530

    Exploring the Base Types

---
+++ Section 30 - Typescript with React/basics.ts
@@ -0,0 +1,13 @@
+//Primitives: number, string,boolean
+// More complex : arrays, objects
+// Function types, parameters
+
+//Primitives
+let age: number;
+age = 12;
+
+let userName: string;
+userName = 'Max';
+
+let isInstructor:boolean;
+isInstructor = true;
