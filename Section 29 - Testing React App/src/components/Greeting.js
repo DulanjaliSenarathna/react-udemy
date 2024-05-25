b339bea55f0f078d641655ff7a34b1c5171d1708
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-25 15:57:52 +0530

    Testing Connected Components

--- Section 29 - Testing React App/src/components/Greeting.js
+++ Section 29 - Testing React App/src/components/Greeting.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () =>{
 
@@ -9,8 +10,8 @@ const Greeting = () =>{
     }
     return <div>
         <h2>Hello World</h2>
-        {!changedText && <p>It's good to see you!</p>}
-        {changedText && <p>Changed!</p>}
+        {!changedText && <Output>It's good to see you!</Output>}
+        {changedText && <Output>Changed!</Output>}
         <button onClick={changeTextHandler}>Click</button>
     </div>
 }
