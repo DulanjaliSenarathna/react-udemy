822a704d0bf856630ddb27479a7f73c5c70436dd
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-25 12:09:42 +0530

    Grouping Tests Together With Test Suites

--- Section 29 - Testing React App/src/components/Greeting.test.js
+++ Section 29 - Testing React App/src/components/Greeting.test.js
@@ -1,15 +1,18 @@
 import { render, screen} from "@testing-library/react"
 import Greeting from "./Greeting"
 
-test('renders Hello world as a text', ()=>{
-    // Arrange
-    render(<Greeting/>)
-
-    //Act
-    //nothing..
-
-    // Assert
-   const helloWorldElement = screen.getByText('Hello World', {exact:false});
-   expect(helloWorldElement).toBeInTheDocument();
-
+describe('Greeting Component',()=>{
+    test('renders Hello world as a text', ()=>{
+        // Arrange
+        render(<Greeting/>)
+    
+        //Act
+        //nothing..
+    
+        // Assert
+       const helloWorldElement = screen.getByText('Hello World', {exact:false});
+       expect(helloWorldElement).toBeInTheDocument();
+    
+    });
 })
+
