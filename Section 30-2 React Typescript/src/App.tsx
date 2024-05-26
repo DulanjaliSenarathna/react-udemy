febfce34e9b05c0716149215fef0e3ff82f96553
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-26 10:25:38 +0530

    Working with Components & TypeScript

---
+++ Section 30-2 React Typescript/src/App.tsx
@@ -0,0 +1,12 @@
+
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+    <Todos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Section 30-2 React Typescript/src/components/Todos.tsx
@@ -0,0 +1,8 @@
+function Todos(){
+return <ul>
+    <li>Learn React</li>
+    <li>Learn Typescript</li>
+</ul>
+}
+
+export default Todos;
