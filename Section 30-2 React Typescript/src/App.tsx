e5ef3d4caf727bcc002052c52a473cb3d390f68a
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-26 13:23:40 +0530

    Adding a Data Model

--- Section 30-2 React Typescript/src/App.tsx
+++ Section 30-2 React Typescript/src/App.tsx
@@ -1,10 +1,16 @@
 
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Java')
+  ]
   return (
     <div>
-    <Todos items={['Learn React','Learn Typescript']}/>
+    <Todos items={todos}/>
     </div>
   );
 }
--- Section 30-2 React Typescript/src/components/Todos.tsx
+++ Section 30-2 React Typescript/src/components/Todos.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Todo from "../models/todo";
 
-const Todos: React.FC<{items: string[]}> = (props) => {
+const Todos: React.FC<{items: Todo[]}> = (props) => {
 return <ul>
-    {props.items.map(item=> <li key={item}>{item}</li>)}
+    {props.items.map(item=> <li key={item.id}>{item.text}</li>)}
 </ul>
 }
 
