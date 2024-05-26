59b282fde5d2b375706ef60ea184685a56135eeb
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-26 12:32:12 +0530

    Working with Props & TypeScript

--- Section 30-2 React Typescript/src/App.tsx
+++ Section 30-2 React Typescript/src/App.tsx
@@ -4,7 +4,7 @@ import Todos from './components/Todos';
 function App() {
   return (
     <div>
-    <Todos/>
+    <Todos items={['Learn React','Learn Typescript']}/>
     </div>
   );
 }
--- Section 30-2 React Typescript/src/components/Todos.tsx
+++ Section 30-2 React Typescript/src/components/Todos.tsx
@@ -1,7 +1,8 @@
-function Todos(){
+import React from "react";
+
+const Todos: React.FC<{items: string[]}> = (props) => {
 return <ul>
-    <li>Learn React</li>
-    <li>Learn Typescript</li>
+    {props.items.map(item=> <li key={item}>{item}</li>)}
 </ul>
 }
 
