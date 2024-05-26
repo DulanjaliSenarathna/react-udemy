4ebbf78d98fa81db409d93a5e35f086a1e31a6ad
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-26 13:43:33 +0530

    Time to Practice: Exercise Time!

--- Section 30-2 React Typescript/src/components/Todos.tsx
+++ Section 30-2 React Typescript/src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{items: Todo[]}> = (props) => {
 return <ul>
-    {props.items.map(item=> <li key={item.id}>{item.text}</li>)}
+    {props.items.map((item)=> <TodoItem key={item.id} text={item.text}/>)}
 </ul>
 }
 
